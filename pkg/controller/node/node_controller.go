61b367793a9debdcd8d7511c49dea6a6aac54a0d
Joseph-Irving <joseph.irving500@gmail.com>
2019-03-20 13:08:02 +0000

    POC

---
+++ pkg/controller/node/node_controller.go
@@ -0,0 +1,105 @@
+/*
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package node
+
+import (
+	"context"
+
+	"github.com/uswitch/nidhogg/pkg/nidhogg"
+	appsv1 "k8s.io/api/apps/v1"
+	corev1 "k8s.io/api/core/v1"
+	"k8s.io/apimachinery/pkg/api/errors"
+	"k8s.io/apimachinery/pkg/runtime"
+	"sigs.k8s.io/controller-runtime/pkg/controller"
+	"sigs.k8s.io/controller-runtime/pkg/handler"
+	"sigs.k8s.io/controller-runtime/pkg/manager"
+	"sigs.k8s.io/controller-runtime/pkg/reconcile"
+	"sigs.k8s.io/controller-runtime/pkg/source"
+)
+
+/**
+* USER ACTION REQUIRED: This is a scaffold file intended for the user to modify with their own Controller
+* business logic.  Delete these comments after modifying this file.*
+ */
+
+// Add creates a new Node Controller and adds it to the Manager with default RBAC. The Manager will set fields on the Controller
+// and Start it when the Manager is Started.
+func Add(mgr manager.Manager) error {
+	return add(mgr, newReconciler(mgr))
+}
+
+// newReconciler returns a new reconcile.Reconciler
+func newReconciler(mgr manager.Manager) reconcile.Reconciler {
+	return &ReconcileNode{handler: nidhogg.NewHandler(mgr.GetClient(), mgr.GetRecorder("nidhogg")), scheme: mgr.GetScheme()}
+}
+
+// add adds a new Controller to mgr with r as the reconcile.Reconciler
+func add(mgr manager.Manager, r reconcile.Reconciler) error {
+	// Create a new controller
+	c, err := controller.New("node-controller", mgr, controller.Options{Reconciler: r})
+	if err != nil {
+		return err
+	}
+
+	// Watch for changes to Node
+	err = c.Watch(&source.Kind{Type: &corev1.Node{}}, &handler.EnqueueRequestForObject{})
+	if err != nil {
+		return err
+	}
+
+	err = c.Watch(&source.Kind{Type: &corev1.Pod{}}, &handler.EnqueueRequestForOwner{
+		IsController: false,
+		OwnerType:    &appsv1.DaemonSet{},
+	})
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+var _ reconcile.Reconciler = &ReconcileNode{}
+
+// ReconcileNode reconciles a Node object
+type ReconcileNode struct {
+	handler *nidhogg.Handler
+	scheme  *runtime.Scheme
+}
+
+// Reconcile reads that state of the cluster for a Node object and makes changes based on the state read
+// and what is in the Node.Spec
+// TODO(user): Modify this Reconcile function to implement your Controller logic.  The scaffolding writes
+// a Deployment as an example
+// +kubebuilder:rbac:groups=core,resources=nodes,verbs=get;list;watch;create;update;patch;delete
+// +kubebuilder:rbac:groups=core,resources=nodes/status,verbs=get;update;patch
+// +kubebuilder:rbac:groups=core,resources=pods,verbs=get;list;watch
+// +kubebuilder:rbac:groups=,resources=events,verbs=create;update;patch
+func (r *ReconcileNode) Reconcile(request reconcile.Request) (reconcile.Result, error) {
+	// Fetch the Node instance
+	instance := &corev1.Node{}
+	err := r.handler.Get(context.TODO(), request.NamespacedName, instance)
+	if err != nil {
+		if errors.IsNotFound(err) {
+			// Object not found, return.  Created objects are automatically garbage collected.
+			// For additional cleanup logic use finalizers.
+			return reconcile.Result{}, nil
+		}
+		// Error reading the object - requeue the request.
+		return reconcile.Result{}, err
+	}
+
+	return r.handler.HandleNode(instance)
+}
---
+++ pkg/nidhogg/handler.go
@@ -0,0 +1,118 @@
+package nidhogg
+
+import (
+	"context"
+	"fmt"
+	"log"
+	"reflect"
+
+	corev1 "k8s.io/api/core/v1"
+	"k8s.io/client-go/tools/record"
+	"sigs.k8s.io/controller-runtime/pkg/client"
+	"sigs.k8s.io/controller-runtime/pkg/reconcile"
+)
+
+// Handler performs the main business logic of the Wave controller
+type Handler struct {
+	client.Client
+	recorder record.EventRecorder
+}
+
+// NewHandler constructs a new instance of Handler
+func NewHandler(c client.Client, r record.EventRecorder) *Handler {
+	return &Handler{Client: c, recorder: r}
+}
+
+// HandleNode works out what taints need to be applied to the node
+func (h *Handler) HandleNode(instance *corev1.Node) (reconcile.Result, error) {
+
+	// Get Pod for node
+	pod, err := h.getDaemonsetPod(instance.Name)
+	if err != nil {
+		return reconcile.Result{}, fmt.Errorf("error fetching pods: %v", err)
+	}
+
+	copy := instance.DeepCopy()
+
+	if pod == nil || podNotReady(pod) {
+		if taintPresent(copy) {
+			return reconcile.Result{}, nil
+		}
+		copy.Spec.Taints = addTaint(copy.Spec.Taints)
+	} else {
+		copy.Spec.Taints = removeTaint(copy.Spec.Taints)
+	}
+
+	if !reflect.DeepEqual(copy, instance) {
+		instance = copy
+		log.Printf("Updating Node %s\n", instance.Name)
+		h.recorder.Eventf(instance, corev1.EventTypeNormal, "TaintsChanged", "Taints updated to %s", instance.Spec.Taints)
+		err = h.Update(context.TODO(), instance)
+		if err != nil {
+			return reconcile.Result{}, err
+		}
+	}
+
+	return reconcile.Result{}, nil
+}
+
+func (h *Handler) getDaemonsetPod(nodeName string) (*corev1.Pod, error) {
+	opts := client.InNamespace("kube-system")
+	pods := &corev1.PodList{}
+	err := h.List(context.TODO(), opts, pods)
+	if err != nil {
+		return nil, err
+	}
+
+	for _, pod := range pods.Items {
+		for _, owner := range pod.OwnerReferences {
+			if owner.Name == "kiam" {
+				if pod.Spec.NodeName == nodeName {
+					return &pod, nil
+				}
+			}
+		}
+	}
+
+	return nil, nil
+}
+
+func podNotReady(pod *corev1.Pod) bool {
+	for _, status := range pod.Status.ContainerStatuses {
+		if status.Ready == false {
+			return true
+		}
+	}
+	return false
+}
+
+func taintPresent(node *corev1.Node) bool {
+
+	for _, taint := range node.Spec.Taints {
+		if taint.Key == "kiam-not-ready" {
+			return true
+		}
+	}
+	return false
+}
+
+func addTaint(taints []corev1.Taint) []corev1.Taint {
+	return append(taints, corev1.Taint{Key: "kiam-not-ready", Effect: corev1.TaintEffectNoSchedule})
+}
+
+func removeTaint(taints []corev1.Taint) []corev1.Taint {
+	newTaints := []corev1.Taint{}
+
+	for _, taint := range taints {
+		if taint.Key == "kiam-not-ready" {
+			continue
+		}
+		newTaints = append(newTaints, taint)
+	}
+
+	if len(newTaints) == 0 {
+		return nil
+	}
+
+	return newTaints
+}
